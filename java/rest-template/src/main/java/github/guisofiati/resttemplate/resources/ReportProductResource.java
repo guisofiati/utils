3c5f940989535b460b8545d5ea767d5478f75b45
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-10-23 11:56:13 -0300

    Java: Lib Rest Template. INSERT, UPDATE and DELETE

--- java/rest-template/src/main/java/github/guisofiati/resttemplate/resources/ReportProductResource.java
+++ java/rest-template/src/main/java/github/guisofiati/resttemplate/resources/ReportProductResource.java
@@ -2,10 +2,16 @@ package github.guisofiati.resttemplate.resources;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +25,9 @@ public class ReportProductResource {
 	@Autowired
 	private ReportProductService service;
 	
+	@Autowired
+	private HttpServletRequest request;
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
@@ -29,5 +38,26 @@ public class ReportProductResource {
 	public ResponseEntity<List<ProductDTO>> findAll() {
 		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		String token = request.getHeader("Authorization");
+		dto = service.insert(dto, token);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		String token = request.getHeader("Authorization");
+		dto = service.update(id, dto, token);
+		return ResponseEntity.ok(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		String token = request.getHeader("Authorization");
+		service.delete(id, token);
+		return ResponseEntity.noContent().build();
 	}
 }
--- java/rest-template/src/main/java/github/guisofiati/resttemplate/services/ReportProductService.java
+++ java/rest-template/src/main/java/github/guisofiati/resttemplate/services/ReportProductService.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -34,9 +36,49 @@ public class ReportProductService {
 		return result.getBody();
 	}
 	
+	// 3o parâmetro é o corpo da requisição
 	public List<ProductDTO> findAll() {
 		ParameterizedTypeReference<PageDTO<ProductDTO>> responseType = new ParameterizedTypeReference<PageDTO<ProductDTO>>() { };
 		ResponseEntity<PageDTO<ProductDTO>> result = restTemplate.exchange(path, HttpMethod.GET, null, responseType);
 		return result.getBody().getContent();
+	}
+	
+	// para fazer insert, update e delete é necessário autenticação (na api que esta sendo consumida)
+	// 3o parâmetro é o corpo da requisição
+	public ProductDTO insert(ProductDTO dto, String bearenToken) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", bearenToken);
+		
+		HttpEntity<ProductDTO> httpEntity = new HttpEntity<>(dto, headers);
+		
+		ResponseEntity<ProductDTO> result = restTemplate.exchange(path, HttpMethod.POST, httpEntity, ProductDTO.class);
+		return result.getBody();
+	}
+	
+	public ProductDTO update(Long id, ProductDTO dto, String bearenToken) {
+		Map<String, String> uriParams = new HashMap<>();
+		uriParams.put("id", id.toString());
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", bearenToken);
+		
+		HttpEntity<ProductDTO> httpEntity = new HttpEntity<>(dto, headers);
+		
+		ResponseEntity<ProductDTO> result = restTemplate.exchange(path + "/{id}", HttpMethod.PUT, httpEntity, ProductDTO.class, uriParams);
+		return result.getBody();
+	}
+	
+	public Integer delete(Long id, String bearenToken) {
+		Map<String, String> uriParams = new HashMap<>();
+		uriParams.put("id", id.toString());
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", bearenToken);
+		
+		HttpEntity<?> httpEntity = new HttpEntity<>(headers);
+	
+		ResponseEntity<Void> result = restTemplate.exchange(path + "/{id}", HttpMethod.DELETE, httpEntity, Void.class, uriParams);
+	
+		return result.getStatusCodeValue();
 	}
 }
